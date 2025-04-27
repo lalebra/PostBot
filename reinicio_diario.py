30ef35176797f80f6ba425dca8ba4794d0e56237
lalebra <lalebra602@gmail.com>
2025-04-27 19:08:33 -0400

    Actualización de main.py y reinicio_diario.py

--- reinicio_diario.py
+++ reinicio_diario.py
@@ -1,15 +1,14 @@
-from discord.ext import tasks
-from datetime import datetime
 import os
 import sys
+from datetime import datetime
+from discord.ext import tasks
 
 def iniciar_reinicio(bot):
     @tasks.loop(minutes=1)
-    async def reiniciar_a_las_8utc():
+    async def reiniciar_a_las_4am_rd():
         ahora = datetime.utcnow()
-        if ahora.hour == 8 and ahora.minute == 0:
+        if ahora.hour == 8 and ahora.minute == 0:  # Esto es 4:00 AM RD, que es 8:00 AM UTC
             print("🔁 Reiniciando bot automáticamente (4:00 AM RD / 8:00 AM UTC)")
-            await bot.close()
-            os.execv(sys.executable, ['python'] + sys.argv)
+            os.execv(sys.executable, [sys.executable] + sys.argv)  # Reemplaza el proceso
 
-    reiniciar_a_las_8utc.start()
+    reiniciar_a_las_4am_rd.start()
